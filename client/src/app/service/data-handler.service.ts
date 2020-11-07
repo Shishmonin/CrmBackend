43653b8a4d6230928aecf1f7eddd372af2f50969
Sergey Shishmonin <shishmonin-s@mail.ru>
2020-11-07 21:37:38 +0300

    view tasks table without filter by category

--- client/src/app/service/data-handler.service.ts
+++ client/src/app/service/data-handler.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import {Category} from "../model/Category";
+import {Task} from "../model/Task";
 import {TestData} from "../data/Testdata";
 
 @Injectable({
@@ -11,6 +12,10 @@ export class DataHandlerService {
 
   getCategories(): Category[] {
     return TestData.categories;
+  }
+
+  getTasks(): Task[] {
+    return TestData.tasks;
   }
 
 }
